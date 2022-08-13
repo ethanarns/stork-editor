3956b5c8890c90b8f267d7c48c4aab616485aa76
Ethan Messinger-Arns <ethanarns95@gmail.com>
2022-08-13 14:30:08 -0700

    Sanity checks in PaletteTable

--- src/popups/PaletteTable.cpp
+++ src/popups/PaletteTable.cpp
@@ -26,7 +26,7 @@ PaletteTable::PaletteTable(QWidget* parent, YidsRom* rom) {
     this->setRowCount(PaletteTable::PALETTE_TABLE_HEIGHT);
     this->horizontalHeader()->hide();
     this->verticalHeader()->hide();
-    
+
     this->setEditTriggers(QAbstractItemView::NoEditTriggers); // Disable text editing
     this->setStyleSheet("QTableView::item {margin: 0;padding: 0;}");
     this->setItemDelegate(new PixelDelegate);
@@ -41,23 +41,32 @@ void PaletteTable::refreshLoadedTiles() {
     for (int paletteIndex = 0; paletteIndex < PaletteTable::PALETTE_TABLE_HEIGHT; paletteIndex++) {
         QByteArray curPalette = this->yidsRom->currentPalettes[paletteIndex];
         for (int colorIndex = 0; colorIndex < PaletteTable::PALETTE_TABLE_WIDTH; colorIndex++) {
-            auto tileItem = this->item(paletteIndex,colorIndex);
-            if (tileItem == nullptr) {
-                tileItem = new QTableWidgetItem();
-            }
             QByteArray fill;
             fill.resize(64);
             for (int i = 0; i < 64; i++) {
                 fill[i] = colorIndex;
             }
-            tileItem->setData(PixelDelegateData::PIXEL_ARRAY_BG2,fill);
-            tileItem->setData(PixelDelegateData::PALETTE_ARRAY_BG2,curPalette);
-            this->setItem(paletteIndex,colorIndex,tileItem);
+            auto tileItem = this->item(paletteIndex,colorIndex);
+            if (tileItem == nullptr) {
+                tileItem = new QTableWidgetItem();
+                tileItem->setData(PixelDelegateData::PIXEL_ARRAY_BG2,fill);
+                tileItem->setData(PixelDelegateData::PALETTE_ARRAY_BG2,curPalette);
+                this->setItem(paletteIndex,colorIndex,tileItem);
+            } else {
+                tileItem->setData(PixelDelegateData::PIXEL_ARRAY_BG2,fill);
+                tileItem->setData(PixelDelegateData::PALETTE_ARRAY_BG2,curPalette);
+            }
         }
     }
 }
 
 void PaletteTable::paletteTableClicked(int row, int column) {
+    auto tileItem = this->item(row,column);
+    if (tileItem == nullptr) {
+        YUtils::printDebug("Palette tile selected has no data",DebugType::WARNING);
+        return;
+    }
+
     // Color WORD
     uint8_t firstByte = this->yidsRom->currentPalettes[row][column*2];
     uint8_t secondByte = this->yidsRom->currentPalettes[row][column*2+1];
