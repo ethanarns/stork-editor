19408e559421af31d024bed4d8c87b6975da6904
Ethan Messinger-Arns <ethanarns95@gmail.com>
2022-08-16 13:10:00 -0700

    FsPacker packs isntructions correctly

--- src/FsPacker.cpp
+++ src/FsPacker.cpp
@@ -1,15 +1,24 @@
 #include "FsPacker.h"
 
 #include "utils.h"
+#include "compression.h"
 
 #include <vector>
 
 std::vector<uint8_t> FsPacker::packInstruction(uint32_t instructionHex, std::vector<uint8_t> internalData, bool lz77compress) {
-    std::vector<uint8_t> result;
+    // Start with the magic hex
+    std::vector<uint8_t> result = YUtils::uint32toVec(instructionHex);
 
-    auto magicVec = YUtils::uint32toVec(instructionHex);
+    if (lz77compress) {
+        // Without the &, internalData is COPIED into this function
+        internalData = YCompression::lzssVectorRecomp(internalData);
+    }
 
-    YUtils::appendVector(result,magicVec);
+    auto dataLength = YUtils::uint32toVec(internalData.size());
+
+    YUtils::appendVector(result,dataLength);
+
+    YUtils::appendVector(result,internalData);
 
     return result;
 }
