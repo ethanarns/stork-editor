f4a9d272311d985d2a328ad59d32368ffaea6ea7
Ethan Messinger-Arns <ethanarns95@gmail.com>
2022-09-24 11:47:05 -0700

    Created lib directory

--- defaultRun.sh
+++ defaultRun.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Delete prior executables
-rm parasol
-rm blz
-rm lzss
-
 # Do separate executables
-gcc src/blz.c -o blz
-gcc src/lzss.c -o lzss
+gcc lib/blz.c -o blz
+gcc lib/lzss.c -o lzss
 
 # Do make
 qmake
