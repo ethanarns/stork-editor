7ca6ac13789898c9f4ba4aed6de6a2ec08a9207c
Ethan Messinger-Arns <ethanarns95@gmail.com>
2024-02-11 15:03:25 -0800

    LayerData fix

--- src/data/MapData.cpp
+++ src/data/MapData.cpp
@@ -7,8 +7,19 @@
 #include <vector>
 #include <fstream>
 
-ScenData::ScenData(std::vector<uint8_t> &mpdzBytes, uint32_t &mpdzIndex) {
-    
+LayerData::LayerData(std::vector<uint8_t> &mpdzBytes, uint32_t &mpdzIndex, uint32_t stop) {
+    while (mpdzIndex < stop) {
+        uint32_t subMagic = YUtils::getUint32FromVec(mpdzBytes,mpdzIndex);
+        mpdzIndex += 4;
+        uint32_t subLength = YUtils::getUint32FromVec(mpdzBytes,mpdzIndex);
+        mpdzIndex += 4;
+        if (subMagic == Constants::INFO_MAGIC_NUM) {
+            auto info = new ScenInfoData(mpdzBytes,mpdzIndex,mpdzIndex+subLength);
+            this->subScenData.push_back(info);
+        } else {
+            mpdzIndex += subLength;
+        }
+    }
 }
 
 MapData::MapData(std::vector<uint8_t> mpdzBytes, bool compressed) {
@@ -33,16 +44,54 @@ MapData::MapData(std::vector<uint8_t> mpdzBytes, bool compressed) {
         uint32_t subLength = YUtils::getUint32FromVec(mpdzBytes,mpdzIndex);
         mpdzIndex += 4;
         if (subMagic == Constants::SCEN_MAGIC_NUM) {
-            ScenData scen = ScenData(mpdzBytes,mpdzIndex);
+            YUtils::printDebug("Handling SCEN",DebugType::VERBOSE);
+            auto scen = new LayerData(mpdzBytes,mpdzIndex,mpdzIndex+subLength);
+            this->subData.push_back(scen);
+            YUtils::printDebug(scen->toString(),DebugType::VERBOSE);
         } else if (subMagic == Constants::GRAD_MAGIC_NUM) {
             YUtils::printDebug("Handling GRAD",DebugType::VERBOSE);
+            mpdzIndex += subLength;
         } else if (subMagic == Constants::SETD_MAGIC_NUM) {
             YUtils::printDebug("Handling SETD",DebugType::VERBOSE);
+            mpdzIndex += subLength;
         } else {
             std::stringstream ssSubNotFound;
             ssSubNotFound << "Unknown MPDZ data: ";
             ssSubNotFound << std::hex << subMagic;
             YUtils::printDebug(ssSubNotFound.str(),DebugType::WARNING);
+            mpdzIndex += subLength;
         }
+    }
+}
+
+ScenInfoData::ScenInfoData(std::vector<uint8_t> &mpdzBytes, uint32_t &mpdzIndex, uint32_t stop) {
+    this->layerWidth = YUtils::getUint16FromVec(mpdzBytes,mpdzIndex);
+    mpdzIndex += 4;
+    this->layerHeight = YUtils::getUint16FromVec(mpdzBytes,mpdzIndex);
+    mpdzIndex += 4;
+    this->bgYoffset = YUtils::getUint32FromVec(mpdzBytes,mpdzIndex);
+    mpdzIndex += 4;
+    this->xScrollOffset = YUtils::getUint32FromVec(mpdzBytes,mpdzIndex);
+    mpdzIndex += 4;
+    this->yScrollOffset = YUtils::getUint32FromVec(mpdzBytes,mpdzIndex);
+    mpdzIndex += 4;
+    this->whichBackground = mpdzBytes.at(mpdzIndex);
+    mpdzIndex++;
+    this->layerOrder = mpdzBytes.at(mpdzIndex);
+    mpdzIndex++;
+    this->unkThird = mpdzBytes.at(mpdzIndex);
+    mpdzIndex++;
+    this->baseBlockMaybe = mpdzBytes.at(mpdzIndex);
+    mpdzIndex++;
+    this->colorModeMaybe = YUtils::getUint32FromVec(mpdzBytes,mpdzIndex);
+    mpdzIndex++;
+    if (mpdzIndex == stop) {
+        YUtils::printDebug("No IMBZ string",DebugType::VERBOSE);
+        return;
+    }
+    auto charFileNoExt = YUtils::getNullTermTextFromVec(mpdzBytes,mpdzIndex);
+    this->imbzFilename = charFileNoExt;
+    while (mpdzIndex % 4 != 0) {
+        mpdzIndex++;
     }
 }
--- src/data/MapData.h
+++ src/data/MapData.h
@@ -15,27 +15,69 @@ public:
     uint32_t magicNumber = 0;
 };
 
-class ScenData : public LevelData {
+class ScenInfoData : public LevelData {
 public:
-    ScenData(std::vector<uint8_t> &mpdzBytes, uint32_t &mpdzIndex);
+    ScenInfoData(std::vector<uint8_t> &mpdzBytes, uint32_t &mpdzIndex, uint32_t stop);
+    virtual std::string toString() {
+        std::stringstream ss;
+        ss << "ScenInfoData {" << std::hex << std::endl;
+        ss << "  Layer Width: 0x" << this->layerWidth << std::endl;
+        ss << "  Layer Height: 0x" << this->layerHeight << std::endl;
+        ss << "  BG Y Offset: 0x" << this->bgYoffset << std::endl;
+        ss << "  X Scroll Offset: 0x" << this->xScrollOffset << std::endl;
+        ss << "  Y Scroll Offset: 0x" << this->yScrollOffset << std::endl;
+        ss << "  Background Index: 0x" << (uint16_t)this->whichBackground << std::endl;
+        ss << "  Layer Order: 0x" << (uint16_t)this->layerOrder << std::endl;
+        ss << "  Unknown 3rd: 0x" << (uint16_t)this->unkThird << std::endl;
+        ss << "  Base Block (Maybe): 0x" << (uint16_t)this->baseBlockMaybe << std::endl;
+        ss << "  Color Mode (Maybe): 0x" << this->colorModeMaybe << std::endl;
+        if (!this->imbzFilename.empty()) {
+            ss << "  IMBZ File: " << this->imbzFilename;
+        }
+        ss << "}";
+        return ss.str();
+    };
+    virtual std::vector<uint8_t> compile() {
+        std::vector<uint8_t> result;
+        result = FsPacker::packInstruction(Constants::INFO_MAGIC_NUM,result,false);
+        return result;
+    };
+    uint32_t magicNumber = Constants::INFO_MAGIC_NUM;
+    // Variables
+    uint16_t layerWidth;
+    uint16_t layerHeight;
+    uint32_t bgYoffset;
+    uint32_t xScrollOffset;
+    uint32_t yScrollOffset;
+    uint8_t whichBackground;
+    uint8_t layerOrder;
+    uint8_t unkThird;
+    uint8_t baseBlockMaybe;
+    uint32_t colorModeMaybe;
+    std::string imbzFilename;
+};
+
+class LayerData : public LevelData {
+public:
+    LayerData(std::vector<uint8_t> &mpdzBytes, uint32_t &mpdzIndex, uint32_t stop);
     std::string toString() {
         std::stringstream ss;
-        ss << "ScenData { Subdata Count: ";
+        ss << "LayerData { Subdata Count: ";
         ss << std::dec << subScenData.size();
         ss << " }";
         return ss.str();
     };
     std::vector<uint8_t> compile() {
         std::vector<uint8_t> result;
-        for (int i = 0; i < subScenData.size(); i++) {
-            auto subCompiled = subScenData.at(i).compile();
+        for (size_t i = 0; i < subScenData.size(); i++) {
+            auto subCompiled = subScenData.at(i)->compile();
             YUtils::appendVector(result,subCompiled);
         }
         result = FsPacker::packInstruction(Constants::SCEN_MAGIC_NUM,result);
         return result;
     };
     uint32_t magicNumber = 0;
-    std::vector<LevelData> subScenData;
+    std::vector<LevelData*> subScenData;
 };
 
 class MapData : public LevelData {
@@ -51,15 +93,15 @@ public:
     };
     std::vector<uint8_t> compile() {
         std::vector<uint8_t> result;
-        for (int i = 0; i < subData.size(); i++) {
-            auto subCompiled = subData.at(i).compile();
+        for (size_t i = 0; i < subData.size(); i++) {
+            auto subCompiled = subData.at(i)->compile();
             YUtils::appendVector(result,subCompiled);
         }
         result = FsPacker::packInstruction(Constants::MPDZ_MAGIC_NUM,result);
         return result;
     };
     // Others
-    ScenData* getScenByBg(int bg);
+    LayerData* getScenByBg(int bg);
 private:    
-    std::vector<LevelData> subData;
+    std::vector<LevelData*> subData;
 };
