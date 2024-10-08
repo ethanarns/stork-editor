583c4266bd8a08a315f8d3094347d9aad7f2e9b1
Ethan Messinger-Arns <ethanarns95@gmail.com>
2024-10-08 13:02:31 -0700

    Organized PATH data fetching

--- src/popups/PathWindow.cpp
+++ src/popups/PathWindow.cpp
@@ -6,6 +6,7 @@
 #include <QVBoxLayout>
 #include <QListWidget>
 #include <QLabel>
+#include <QSpinBox>
 
 #include <sstream>
 
@@ -28,17 +29,30 @@ PathWindow::PathWindow(QWidget *parent, YidsRom *rom, GridOverlay* gOverlay) {
     auto leftColumnTitle = new QLabel("Paths",this);
     leftColumn->addWidget(leftColumnTitle);
     mainLayout->addLayout(leftColumn);
+
     auto rightColumn = new QVBoxLayout(this);
     auto rightColumnTitle = new QLabel("Path Points",this);
     rightColumn->addWidget(rightColumnTitle);
+    auto locationsLayout = new QHBoxLayout(this);
+    this->xSpinBox = new QSpinBox(this);
+    this->xSpinBox->setMinimum(0);
+    this->xSpinBox->setSingleStep(1);
+    locationsLayout->addWidget(xSpinBox);
+    this->ySpinBox = new QSpinBox(this);
+    this->ySpinBox->setMinimum(0);
+    this->ySpinBox->setSingleStep(1);
+    locationsLayout->addWidget(ySpinBox);
+
     mainLayout->addLayout(rightColumn);
 
     this->pathListWidget = new QListWidget(this);
     leftColumn->addWidget(this->pathListWidget);
     this->pointListWidget = new QListWidget(this);
     rightColumn->addWidget(this->pointListWidget);
+    rightColumn->addLayout(locationsLayout);
 
     connect(this->pathListWidget,&QListWidget::currentRowChanged,this,&PathWindow::pathListRowSelectionChanged);
+    connect(this->pointListWidget,&QListWidget::currentRowChanged,this,&PathWindow::pointSelectionChanged);
 }
 
 void PathWindow::refreshPathList() {
@@ -50,7 +64,6 @@ void PathWindow::refreshPathList() {
         // Holds them as pointers
         delete this->pathListWidget->takeItem(0);
     }
-
     this->pathListWidget->clear();
 
     if (this->yidsRom->mapData == nullptr) {
@@ -84,40 +97,16 @@ void PathWindow::refreshPathList() {
 
 void PathWindow::refreshPointList() {
     this->detectChanges = false;
+    // Wipe the existing list
     while (this->pointListWidget->count() > 0) {
         delete this->pointListWidget->takeItem(0);
     }
     this->pointListWidget->clear();
 
-    auto pathIndex = this->pathListWidget->currentRow();
-    if (pathIndex == -1) {
-        //YUtils::printDebug("No Path selected",DebugType::WARNING);
-        this->detectChanges = true;
-        return;
-    }
+    // Get the path data from PATH
+    auto pathDataSelected = this->getSelectedPathData();
 
-    // Get data
-    if (this->yidsRom->mapData == nullptr) {
-        YUtils::printDebug("MapData is null",DebugType::ERROR);
-        this->detectChanges = true; // Just in case
-        return;
-    }
-    auto pathDataMaybe = this->yidsRom->mapData->getFirstDataByMagic(Constants::PATH_MAGIC_NUM,true);
-    if (pathDataMaybe == nullptr) {
-        YUtils::printDebug("No PATH data found in refreshPointList",DebugType::WARNING);
-        this->detectChanges = true;
-        return;
-    }
-    auto pathData = static_cast<PathData*>(pathDataMaybe);
-
-    if (pathIndex >= (int)pathData->paths.size()) {
-        YUtils::printDebug("Seeking Path data out of bounds",DebugType::ERROR);
-        this->detectChanges = true;
-        return;
-    }
-
-    auto pathDataSelected = pathData->paths.at(pathIndex);
-
+    // Refill the list
     uint pointIndex = 0;
     for (auto pit = pathDataSelected.cbegin(); pit != pathDataSelected.cend(); pit++) {
         auto point = (*pit);
@@ -132,20 +121,114 @@ void PathWindow::refreshPointList() {
 }
 
 void PathWindow::pathListRowSelectionChanged(int rowIndex) {
+    this->detectChanges = false;
     //std::cout << "pathListRowSelectionChanged " << rowIndex << std::endl;
     this->refreshPointList();
     if (rowIndex >= this->pathListWidget->count()) {
         YUtils::printDebug("rowIndex too high in pathListRowSelectionChanged",DebugType::ERROR);
+        this->detectChanges = true;
         return;
     }
     if (rowIndex < -1) {
         YUtils::printDebug("Unusually negative rowIndex",DebugType::ERROR);
+        this->detectChanges = true;
         return;
     }
     if (this->gridOverlay == nullptr) {
         YUtils::printDebug("gridOverlay was null",DebugType::ERROR);
+        this->detectChanges = true;
         return;
     }
     this->gridOverlay->selectedPathIndex = rowIndex;
     this->gridOverlay->repaint();
+    this->detectChanges = true;
+}
+
+void PathWindow::pointSelectionChanged(int rowIndex) {
+    if (this->detectChanges == false) {
+        return;
+    }
+    // Debug stuff
+    std::cout << "pointSelectionChanged: " << rowIndex << std::endl;
+    if (rowIndex < -1) {
+        YUtils::printDebug("rowIndex too low, below -1 somehow", DebugType::ERROR);
+        return;
+    }
+    if (rowIndex >= this->pointListWidget->count()) {
+        YUtils::printDebug("rowIndex too high in pointSelectionChanged",DebugType::ERROR);
+        return;
+    }
+
+    // Retrieve the major/parent path data within PATH for this map
+    auto selectedPath = this->getSelectedPathData();
+
+    // Update GridOverlay
+    if (this->gridOverlay == nullptr) {
+        YUtils::printDebug("gridOverlay was null",DebugType::ERROR);
+        return;
+    }
+    this->gridOverlay->selectedPathSubIndex = rowIndex;
+    this->gridOverlay->repaint();
+}
+
+std::vector<PathSection *> PathWindow::getSelectedPathData() {
+    if (this->yidsRom->mapData == nullptr) {
+        YUtils::printDebug("MapData is null",DebugType::ERROR);
+        return std::vector<PathSection *>(); // Return empty
+    }
+    int rowIndex = this->pathListWidget->currentRow();
+    if (rowIndex >= this->pathListWidget->count()) {
+        YUtils::printDebug("rowIndex too high in getSelectedPathData",DebugType::ERROR);
+        return std::vector<PathSection *>(); // Return empty
+    }
+    if (rowIndex < 0) {
+        // Fail silently, there just isn't anything selected
+        return std::vector<PathSection *>();
+    }
+    // Retrieve the PATH data for this map
+    auto pathDataMaybe = this->yidsRom->mapData->getFirstDataByMagic(Constants::PATH_MAGIC_NUM,true);
+    if (pathDataMaybe == nullptr) {
+        YUtils::printDebug("No PATH data found in getSelectedPathData",DebugType::WARNING);
+        this->detectChanges = true;
+        return std::vector<PathSection *>();
+    }
+    auto pathData = static_cast<PathData*>(pathDataMaybe);
+
+    if (rowIndex >= (int)pathData->paths.size()) {
+        YUtils::printDebug("Seeking Path data out of bounds in getSelectedPathData",DebugType::ERROR);
+        return std::vector<PathSection *>();
+    }
+    auto pathDataSelected = pathData->paths.at(rowIndex);
+    return pathDataSelected;
+}
+
+PathSection* PathWindow::getSelectedPathPoint() {
+    // Get the row properly
+    int rowIndex = this->pointListWidget->currentRow();
+    if (rowIndex >= this->pathListWidget->count()) {
+        YUtils::printDebug("rowIndex too high in getSelectedPathPoint",DebugType::ERROR);
+        return nullptr; // Return empty
+    }
+    // This should never be called if there is nothing selected, check prior
+    if (rowIndex < 0) {
+        YUtils::printDebug("negative rowIndex in getSelectedPathPoint",DebugType::ERROR);
+        return nullptr;
+    }
+
+    auto pathData = this->getSelectedPathData();
+    if (pathData.size() == 0) {
+        YUtils::printDebug("No selected path data found in getSelectedPathPoint",DebugType::WARNING);
+        return nullptr;
+    }
+    // Safe to cast to unsigned as negative check was earlier
+    if ((unsigned int)rowIndex >= pathData.size()) {
+        YUtils::printDebug("point index selected is out of bounds in actual point list",DebugType::ERROR);
+        return nullptr;
+    }
+    auto pointSelected = pathData.at(rowIndex);
+    if (pointSelected == nullptr) {
+        YUtils::printDebug("pointSelected is null",DebugType::ERROR);
+        return nullptr;
+    }
+    return pointSelected;
 }
--- src/popups/PathWindow.h
+++ src/popups/PathWindow.h
@@ -2,6 +2,7 @@
 
 #include "../yidsrom.h"
 #include "../GridOverlay.h"
+#include "qspinbox.h"
 
 #include <QtCore>
 #include <QWidget>
@@ -13,11 +14,17 @@ public:
     PathWindow(QWidget *parent, YidsRom* rom, GridOverlay* gOverlay);
     QListWidget* pathListWidget;
     QListWidget* pointListWidget;
+    QSpinBox* xSpinBox;
+    QSpinBox* ySpinBox;
     void refreshPathList();
     void refreshPointList();
     void pathListRowSelectionChanged(int rowIndex);
+    void pointSelectionChanged(int rowIndex);
 private:
     YidsRom* yidsRom;
     GridOverlay* gridOverlay;
     bool detectChanges = true;
+
+    std::vector<PathSection *> getSelectedPathData();
+    PathSection* getSelectedPathPoint();
 };
